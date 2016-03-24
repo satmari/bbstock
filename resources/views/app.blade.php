80e740394c9f7abf37ba0b5f7d43e19de47ff2cd
satmari <satmariatila@gmail.com>
2016-03-24 12:10:52 +0100

    2016.03.24

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,7 +27,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="{{ url('/') }}">BBStock Application</a>
+				<a class="navbar-brand" href="{{ url('/') }}"><b>BBStock Application</b></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">

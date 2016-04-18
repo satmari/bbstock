f861daba8cabe52f1c8b13a2dd4a13cfef986340
satmari <satmariatila@gmail.com>
2016-04-18 09:35:16 +0200

    2016.04.17

--- resources/views/map/showtable.blade.php
+++ resources/views/map/showtable.blade.php
@@ -5,16 +5,19 @@
 <div class="container container-table">
 <big><b>All Boxes</b></big>	
 
-<table class="table table-bordered table-condensed">
+<div class="input-group"> <span class="input-group-addon">Filter</span>
+	<input id="filter" type="text" class="form-control" placeholder="Type here...">
+</div>
+<table class="table table-condensed table-bordered" id="sort" >
 	<thead>
 		<tr>
-			<th>BB name</th>
+			<th data-sortable="true" >BB name</th>
 			<th>Qty of BB</th>
 			<th></th>
 		</tr>
 		
 	</thead>
-	<tbody>
+	<tbody class="searchable">
 		@foreach ($bbstock as $bb)	
 		<tr>
 			<td>{{ substr($bb->bbname, -8)  }}</td>

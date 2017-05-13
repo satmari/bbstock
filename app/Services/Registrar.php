461f92d7c264f47bc933949f6ee1bc03732b02f7
satmari <satmariatila@gmail.com>
2017-05-13 14:13:53 +0200

    2017.05.13

--- app/Services/Registrar.php
+++ app/Services/Registrar.php
@@ -15,8 +15,8 @@ class Registrar implements RegistrarContract {
 	public function validator(array $data)
 	{
 		return Validator::make($data, [
-			'name' => 'required|max:255',
-			'email' => 'required|email|max:255|unique:users',
+			'name' => 'required|max:25',
+			'email' => 'required|max:25',
 			'password' => 'required|confirmed|min:6',
 		]);
 	}

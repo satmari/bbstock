56c17eca8d8b5acc34e1f7937fd2d2135173ed90
satmari <satmariatila@gmail.com>
2018-10-25 10:51:41 +0200

    2018.10.25

--- app/Http/Controllers/inteosdbController.php
+++ app/Http/Controllers/inteosdbController.php
@@ -92,7 +92,20 @@ class inteosdbController extends Controller {
 		$ClrDesc = $inteos_array[0]['ClrDesc'];
 		$StyCod =  $inteos_array[0]['StyCod'];
 		
-		list($ColorCode, $Size) = explode('-', $Variant); 
+		// list($ColorCode, $Size) = explode('-', $Variant); 
+
+		$brlinija = substr_count($Variant,"-");
+			// echo $brlinija." ";
+
+			if ($brlinija == 2)
+			{
+				list($ColorCode, $size1, $size2) = explode('-', $Variant);
+				$Size = $size1."-".$size2;
+				// echo $color." ".$size;	
+			} else {
+				list($ColorCode, $Size) = explode('-', $Variant);
+				// echo $color." ".$size;
+			}
 	
 		//return view('welcome', compact('bbstock', 'inteos'));
 		//return view('welcome', compact('IntKeyPO', 'BlueBoxNum'));
